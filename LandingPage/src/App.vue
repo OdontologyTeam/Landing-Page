aed84688b61dce9f53859ecf55ec08d007ae8b85
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-20 19:43:01 -0500

    UPDATE: adding the navbar

--- LandingPage/src/App.vue
+++ LandingPage/src/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="app">
-        
+        <div class="contenedor">
+            <nav>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Ubicación</a></li>
+                <li><a href="#">Contacto</a></li>
+            </nav>
+        </div>
     </div>   
 </template>
 <script>

46e09df221035892e31640082faae09cb7c0251d
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-21 20:07:47 -0500

    ADD:  content in Home component

--- LandingPage/src/App.vue
+++ LandingPage/src/App.vue
@@ -5,15 +5,20 @@
                 <li v-for="(item, index) in navItems" key="index" :item="item"><a href="#">{{item}}</a></li>               
             </nav>
         </div>
+        <Home/> 
     </div>   
 </template>
 <script>
+    import Home from './components/Home.vue'
     export default{
         name: 'app',
         data(){
             return{
                 navItems: ['Inicio', 'Acerca de', 'Servicios', 'Ubicación', 'Contacto']
             }
+        },
+        components:{
+            Home
         }
     }
 </script>
--- LandingPage/src/components/Home.vue
+++ LandingPage/src/components/Home.vue
@@ -1,8 +1,32 @@
 <template>
-    
+  <div class="contenedor">
+    <div class="padre">
+      <div class="izq">
+        <h1>{{title}}</h1>
+        <p>{{text}}</p>
+        <button>{{button}}</button>
+      </div>
+      <div class="der">
+        <img src="imgs/dentist.png" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-    export default{
-        
+  export default {
+    data() {
+      return {
+          title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, ad! Quod expedita libero dicta
+          numquam ducimus, error iure mollitia, aperiam eaque ipsa illo, molestiae maxime architecto!
+          Vero, necessitatibus! Corrupti, qui?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, ad! Quod expedita libero dicta
+          numquam ducimus, error
+          iure mollitia, aperiam eaque ipsa illo, molestiae maxime architecto! Vero, necessitatibus!
+          Corrupti, qui?`,
+          button: 'Empezar ya'
+      }
     }
+  }
+
 </script>

1c50a91a3daf91b22924aa14a097ca9f6a030b78
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-22 20:46:10 -0500

    UPDATE: creating the HTML of location

--- LandingPage/src/components/Location.vue
+++ LandingPage/src/components/Location.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1626.8980574358104!2d-73.1129981704048!3d7.119810670076908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e683fdf934d3f4f%3A0xe2b76c017a692061!2sEdificio+Tempo+I+I!5e0!3m2!1ses-419!2sco!4v1542936565090" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div class="box">
+            <div class="content">
+                <div class="tit">
+                    <div class="icon"></div>
+                    <h3 class="title"></h3>        
+                </div>
+                <label for="" class="address"></label>
+                <label for="" class="phone"></label>
+                <hr>
+                <div class="schedule">
+                    <p class="text"><span class="time"></span></p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>

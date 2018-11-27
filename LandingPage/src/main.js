aaf5c5812ba4bb45f42731c51b02887a6164f48a
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-26 19:20:20 -0500

    UPDATE: nothing

--- LandingPage/src/main.js
+++ LandingPage/src/main.js
@@ -2,10 +2,10 @@ import vue from 'vue'
 import App from './App.vue'
 
 new vue({
-    el: '#app',
-    render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
 
 window.addEventListener("load", function (event) {
-    document.getElementById('loading').style.display = "none";
-});
+  document.getElementById('loading').style.display = "none";
+})

d8284499e7b97c1283c098c5b34370d833b0fd90
teamzz111 <aflr@outlook.com>
2018-12-04 22:43:26 -0500

    Responsive Menu

--- LandingPage/src/App.vue
+++ LandingPage/src/App.vue
@@ -4,7 +4,20 @@
       <div class="lds-hourglass">
       </div>
     </section>
-    <header>
+    <header class="responsive">
+      <div class="barraFixed">
+        <div class="izquierdo">LOGOTIPO</div>
+        <div class="derecho"><i class="fas fa-bars"></i></div>
+      </div>
+      <div class="contenedor">
+        <nav>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Acerca</a></li>
+          <li><a href="#">Contacto</a></li>
+        </nav>
+      </div>
+    </header>
+    <header class = "normal">
       <div class="contenedor">
         <nav>
           <li v-for="item in navItems" :item="item"><a href="#">{{item}}</a></li>
@@ -30,7 +43,7 @@
       <Contact />
     </section>
     <section class="foot-sect">
-      <Foot/>
+      <Foot />
     </section>
   </div>
 </template>
@@ -58,4 +71,5 @@
       Foot
     }
   }
+
 </script>

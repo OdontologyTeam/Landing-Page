bd6143ab640db6ff596d483bc0f124521f8a39fc
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-19 20:40:44 -0500

    UPDATE: adding services

--- LandingPage/src/components/Services.vue
+++ LandingPage/src/components/Services.vue
@@ -2,7 +2,17 @@
   <div class="contenedor">
     <h2>Conoce mis servicios</h2>
     <div class="padre" data-aos="zoom-in-down">
-      <div class="caja" v-for="info in cardData">
+      <div class="caja" v-for="info in mainServices">
+        <div class="img">
+            <img :src="'../../imgs/'+info.img" alt="">
+        </div>
+        <div class="titulo">
+          <h3>{{info.title}}</h3>
+          <p> {{info.content}} </p>
+        </div>
+        <button>Ver más</button>
+      </div>
+      <div class="caja" v-for="info in secondServices">
         <div class="img">
             <img :src="'../../imgs/'+info.img" alt="">
         </div>
@@ -20,7 +30,7 @@
   export default {
     data() {
       return {
-          cardData: [
+          mainServices: [
               {
                   img:'molar.png',
                   title: 'Endodoncia',
@@ -42,6 +52,29 @@
                             expedita perspiciatis
                             labore`
               }
+          ],
+          secondServices:[
+            {
+              img:'mouth.png',
+              title: 'Rehabilitación Oral',
+              content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur, explicabo itaque
+                            expedita perspiciatis
+                            labore`
+            },
+            {
+              img:'gum.png',
+              title: 'Periodoncia',
+              content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur, explicabo itaque
+                            expedita perspiciatis
+                            labore`
+            },
+            {
+              img:'tooth.png',
+              title: 'Cirugía',
+              content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non aspernatur, explicabo itaque
+                            expedita perspiciatis
+                            labore`
+            }
           ]
       }
     }
